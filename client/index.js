a1a977abb831baaa7612a6a5368efef1d9bd8389
EmLmr <emeline.coding@gmail.com>
2022-04-11 10:48:06 +0200

    Added dynamic row display when no data available

---
+++ client/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadHTMLtable([]);
+});
+
+function loadHTMLtable(data) {
+    const table = document.querySelector('table tbody');
+
+    if (data.length === 0) table.innerHTML = '<tr><td class="no-data" colspan="5">No data</tr></td>';
+}
