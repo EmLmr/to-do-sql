9b80055db4b49df64a7d31d710e957eb42b885fc
EmLmr <emeline.coding@gmail.com>
2022-04-11 15:27:32 +0200

    Setup database connection + created dbService.js + created Read operation

--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { response } = require('express');
 dotenv.config();
+
+const dbService = require('./dbService'); // imported, is an object
 
 // To make API calls.
 app.use(cors());
@@ -11,3 +14,13 @@ app.use(cors());
 app.use(express.json());
 // Not sending any form data.
 app.use(express.urlencoded({ extended: false }));
+
+// READ.
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    result.then((data) => response.json({ data: data })).catch((err) => console.log(err));
+});
+
+app.listen(process.env.PORT, () => console.log('To-do app is running'));
